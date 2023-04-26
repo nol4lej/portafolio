c0c959e083fcb91ac0daec09fe073546d219de93
nol4lej <nome.ahumada@outlook.com>
2023-04-25 21:52:33 -0400

    Nav responsive added

---
+++ src/js/navbar.js
@@ -0,0 +1,29 @@
+// const navbarCollapsible = document.querySelector('nav');
+const navbarCollapsible = document.getElementById('menu-rpsv');
+const btn_rpsv = document.getElementById("btn-rpsv");
+const menu_rpsv = document.getElementById("menu-rpsv")
+
+window.addEventListener("scroll", function(){
+    if (!navbarCollapsible) {
+        return;
+    } if (window.scrollY === 0) {
+        navbarCollapsible.classList.remove('navbar-shrink')
+    } else {
+        navbarCollapsible.classList.add('navbar-shrink')
+    }
+})
+
+window.addEventListener("load", function(){
+    if(window.pageYOffset > 1){
+        navbarCollapsible.classList.add('navbar-shrink')
+    }
+})
+
+const visibility = () =>{
+    if(menu_rpsv.classList.contains("hidden")){
+        menu_rpsv.classList.replace("hidden", "block");
+        menu_rpsv.classList.add("no-rspv");
+    } else {
+        menu_rpsv.classList.replace("block", "hidden");
+    }
+}
